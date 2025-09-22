3c90daf33ab6c7739bab039fcd12e4aecea79b0c
Pooja0629 <poojashree2266@gmail.com>
2025-09-22 21:46:04 +0530

    Update demand_analysis.py

--- pages/demand_analysis.py
+++ pages/demand_analysis.py
@@ -1,8 +1,8 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from calculations import *
-from model import get_forecast
+from .calculations import *
+from .model import get_forecast
 import streamlit as st
 import pandas as pd
 import plotly.express as px
